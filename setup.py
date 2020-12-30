2719643d5fa077d7a5a657ec7df9ae0b58264308
karthikeyansa <karthikeyansa39@gmail.com>
2020-12-30 16:14:26 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import find_packages, setup
 
+with open('requirements.txt','r') as file:
+    requirements = file.read().splitlines()
+
 setup(
     name='jsonfile_crd',
     packages=find_packages(include=['jsonfile_crd']),
-    version='1.0.6',
+    version='1.0.1',
     description='Libraries to perform CRD over JSON file',
     author='Karthikeyan',
     authors_email=['karthikeyansa39@gmail.com'],
-    install_requires=[],
+    install_requires=requirements,
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
